96d3f81351b89c4bf4b1df20bfc7a2415707a0e3
freeitxiaofei <freeitxiaofei@gmail.com>
2016-12-07 01:57:54 +0800

    updates161207

--- paramiko/paramiko.py
+++ paramiko/paramiko.py
@@ -1,13 +1,38 @@
 __author__ = 'Administrator'
 #-*-coding:utf-8-*-
-import sys,paramiko
+import sys
+import paramiko
+import getpass
+import socket
 
-host = sys.argv[1] #定义一个ip地址，例如：paramiko 192.168.1.254
+#host = sys.argv[1] #定义一个ip地址，例如：paramiko 192.168.1.254
 #为什么是sys.argv[1]而不是sys.argv[0]呢？
 #答：sys.argv[0]在shell中位置参数为第一位，表示程序本身，例如：执行./paramiko 192.168.1.254 ifconfig 其中，paramiko本身占用一个位置参数
-user = 'user'
-password = 'xiaofei'
-cmd = sys.argv[2] #定义远程执行命令
+user = ''
+port = 22
+if len( sys.argv) > 1:
+    hostname = sys.argv[1]
+    if hostname.find('@') >= 0:
+        user,hostname = hostname.split('@')
+    elif hostname.find(':') >= 0:
+        hostname,portstr = hostname.split(':')
+        port = int(portstr)
+else:
+    hostname = raw_input('Hostname: ')
+if len(hostname) == 0:
+    print ('Error: not hostname')
+    sys.exit(1)
+if user == '':
+    default_user = getpass.getuser()
+    user = raw_input('User %s' %default_user)
+    if len(user) == 0:
+        print 'Unkown username'
+        sys.exit(1)
+try:
+    sock = socket.(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect #未完成
+
+#cmd = sys.argv[2] #定义远程执行命令
 s = paramiko.SSHClient() #绑定连接实例对象
 s.load_system_host_keys() #加载本机HOST主机文件
 s.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #
